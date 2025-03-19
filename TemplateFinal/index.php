653593ec0d2b9e58cc8be95ddf6bfaae9d4c71a9
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-03-19 21:12:40 +0700

    uppppp

---
+++ TemplateFinal/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang thông tin Bình Định</title>
+    <link rel="stylesheet" href="scr/main.css">
+    <link rel="stylesheet" href="scr/reset.css">
+</head>
+<body>
+    <header class="header" id="head-container">
+        <div class="header title" id = "head-title">
+            <img class="header title logo" src="scr/img/Logo-Tinh-Binh-Dinh.webp" alt="">
+            <div class="des-container" >
+                <h1 class="des-container h1">Cổng thông tin Bình Định</h1>
+                <label class="des-container slogan">Nhanh - Chính xác - Trách nhiệm</label>
+            </div>
+        </div>
+        <div class="header button">
+            <button class="header button login"> Đăng nhập </button>
+            <button class="header button res">Đăng ký</button>
+        </div>
+    </header>
+    <nav class="nav" id="nav-container">
+        <ul class="nav ul">
+            <li class="nav ul li"><a href="#"> Trang chủ </a></li>
+            <li class="nav ul li"><a href="#"> Mới nhất </a></li>
+            <li class="nav ul li"><a href="#"> Nóng nhất </a></li>
+            <li class="nav ul li"><a href="#">Thời sự </a></li>
+            <li class="nav ul li"><a href="#">Chính quyền </a></li>
+            <li class="nav ul li"><a href="#">Văn hóa - xã hội </a></li>
+            <li class="nav ul li"><a href="#">Giao thông </a></li>
+            <li class="nav ul li"><a href="#">Du lịch Bình Định </a></li>
+        </ul>
+    </nav>
+    <div class="body-container">
+        <div class="content" id = "content-container">
+            <div class="content first" >
+                <div class="content first left">
+                    <h1 class="content-title">Tin nóng</h1>
+                    <hr class="hr-divide">
+                    <div class="content first left hotnews">
+                        <img src="https://cdn2.tuoitre.vn/thumb_w/480/471584752817336320/2024/12/11/edit-46980599211551147528521984309402575554985839n-1733889662255711833361.jpeg" alt="">
+                        <i>Thứ 3 ngày 12 tháng 3 năm 2025</i>
+                        <h2 for="">Cộng đồng mạng sai rồi ! Jack không hề bỏ con như lời đồn</h2>
+                    </div>
+                </div>
+                <div class="content first right">
+                    <h1 class="content-title">Tin mới</h1>
+                    <hr class="hr-divide">
+                    <div class="content first right news" id="right-new-container">
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Sau khi sự thật về Jack được công bố, Thiên An đã khóa tất cả mạng xã hội</h3>
+                                <i>Thứ 3 ngày 12 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Chơi genshin quá 180p, một người đã rơi từ tầng 81 xuống đất chỉ vì tưởng rằng mình có phong chi dực</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Bán cả gia tài đầu tư lấy C6R5 Furina, thanh niên ở Bình Dương bị bắt sang Campuchia để lừa đảo vì tìm việc nhẹ lương cao.</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Bốc đầu tạt đầu xe tải, 3 nam thanh niên nhận cái kết đắng</h3>
+                                <i>Thứ 6 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Lợi ích bất ngờ của việc yêu người đồng tính không phải ai cũng biết</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+                        <div class="content first right news content">
+                            <a href="#">
+                                <h3>Phát hiện chấn động: Người mọc sừng ở Đồng Nai</h3>
+                                <i>Thứ 3 ngày 13 tháng 3 năm 2025</i>
+                            </a>
+                            <hr class="hr-new-divide">
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+            <div class="content second">
+                <div></div>
+            </div>
+            <div class="content third"></div>
+            <div class="content fourth"></div>
+            <div></div>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
